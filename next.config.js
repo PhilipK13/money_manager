7417369636ee2fe23d43cc7a14030d1f8ca67c51
PhilipK13 <67237885+PhilipK13@users.noreply.github.com>
2022-07-07 21:43:11 -0400

    added thing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+/* eslint-disable require-await */
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+const prod = process.env.NODE_ENV === "production";
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
   pwa: {
-    dest: 'public',
+    disable: prod ? false : true,
+    dest: "public",
     runtimeCaching,
   },
-})
+});
+
+module.exports = nextConfig;
