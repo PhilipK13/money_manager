23f84a04f19ab0abd223f875c3cdf39f97806d26
PhilipK13 <67237885+PhilipK13@users.noreply.github.com>
2022-07-27 01:12:37 -0400

    Updated Transaction creation functionality
    Transactions are now split based on what users in the group are selected.

--- client/pages/groups/index.tsx
+++ client/pages/groups/index.tsx
@@ -6,23 +6,24 @@ import '@aws-amplify/ui-react/styles.css';
 import getToken from '../../utils/getToken';
 import axios from 'axios';
 
-
 export default function groups() {
 
   const router = useRouter();
 
   type Group = {
-    id: Key;
+    id: number;
     name: string;
     description: string;
   };
 
   const {user} = useAuthenticator();
 
+  const [myMap, setMyMap] = useState(new Map());
   const [usersCurrentGroups, setUsersCurrentGroups] = useState([]);
-  const [showGroupCreate, setShowGroupCreate] = useState(false)
-  const [groupName, setGroupName] = useState('')
-  const [groupDescription, setGroupDescription] = useState('')
+  const [showGroupCreate, setShowGroupCreate] = useState(false);
+  const [disableGroupCreate, setDisableGroupCreate] = useState(false);
+  const [groupName, setGroupName] = useState('');
+  const [groupDescription, setGroupDescription] = useState('');
 
   var lock = 0;
 
@@ -34,6 +35,26 @@ export default function groups() {
     }
   }, [user]);
  
+  //Gets the users associated with groups
+  const getUsersFromGroup = async (groups: any) => {
+    
+    for(let i = 0; i < groups.length; i++) {
+      try {
+        const resp = await axios.post(
+          `/api/groups/getUsersFromGroup`,
+          { group_id: groups[i].id },
+          { headers: { Authorization: `Bearer ${getToken(user)}` } }
+        );
+        setMyMap(myMap.set(groups[i].id, resp.data));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    
+    console.log(myMap);
+    
+  }
+
   //Gets the users groups from the database
   const getUsersGroups = async () => {
     try {
@@ -41,16 +62,33 @@ export default function groups() {
         `/api/groups/retrieve`,
         { headers: { Authorization: `Bearer ${getToken(user)}` } }
       );
+      await getUsersFromGroup(resp.data);
       setUsersCurrentGroups(resp.data);
-      console.log(resp.data);
+      
     } catch (err) {
       console.log(err);
     }
   }
 
+
+  //Returns the picture for all the users in the group
+  const displayUsers = (groupId: number) => {
+    console.log(groupId)
+    var users = myMap.get(groupId);
+    console.log(users);
+    return (
+      <div className='flex flex-row space-x-4'>
+        {users.map((user: any) => (
+          <img className="rounded-full" src={user.picture} alt={user.name} width="50px"/>
+        ))}
+      </div>
+    )
+  }
+
   //Creates a new group associated with the user
   const createGroup = async (e:  MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    setDisableGroupCreate(true);
     try {
       const resp = await axios.post(
         `/api/groups/create`,
@@ -63,8 +101,9 @@ export default function groups() {
       console.log(resp);
       console.log(resp.status);
       if(resp.status === 200) {
-        getUsersGroups();  
+        getUsersGroups();
         setShowGroupCreate(false);
+        setDisableGroupCreate(false);
       }
     } catch (err) {
       console.log(err);
@@ -82,19 +121,22 @@ export default function groups() {
       <div className="flex flex-col space-y-4">
         {/* Displays all the groups the user is in using the name in the order of id */}
         {usersCurrentGroups.map((group: Group) => (
+
           <a 
             className="flex flex-col space-y-4 border-cyan-800 hover:cursor-pointer border" 
             key={group.id}
             onClick={() => {
               router.push({
-                pathname: `../transactions/`,
+                pathname: `/transactions`,
                 query: { groupid: group.id },
-                
               })
             }}
           >
-            <h2>{group.name}</h2>
-            <p>{group.description}</p>
+            {console.log("Count")}
+            <h2>Name: {group.name}</h2>
+            <p>Description: {group.description}</p>
+            {/* display the users names from the userlist map that has a key of the group id */}
+            {displayUsers(group.id)}
           </a>
         ))}
       </div>
@@ -117,6 +159,7 @@ export default function groups() {
           />
           <Button
             onClick={(e: MouseEvent<HTMLButtonElement>) => {createGroup(e)}}
+            disabled={disableGroupCreate}
           >
             Create Group
           </Button>
--- client/pages/transactions/index.tsx
+++ client/pages/transactions/index.tsx
@@ -1,13 +1,14 @@
 import styles from '../../styles/Home.module.css'
 import { useState, ChangeEvent, MouseEvent, useEffect, Key } from 'react';
 import { useRouter } from 'next/router'
-import { Button, useAuthenticator, TextField } from '@aws-amplify/ui-react';
+import { Button, useAuthenticator, TextField, SwitchField } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import axios from 'axios';
 
 import getToken from '../../utils/getToken';
 import getUserAttributes from '../../utils/getUserAttributes';
+import { AiOutlineLoading3Quarters as LoadingIcon } from 'react-icons/ai';
 
 
 type Transaction = {
@@ -23,7 +24,12 @@ type TransactionSplit = {
   share: number;
 }
 
-
+interface User {
+  id: string,
+  name: string,
+  email: string,
+  picture: string
+}
 
 export default function transactions() {
 
@@ -38,11 +44,18 @@ export default function transactions() {
     share: number;
   }
 
+  
+  const [groupUsers, setGroupUsers] = useState<any>([]);
+  const [groupUsersSplitting, setGroupUsersSpltting] = useState<any>([]);
   const [transactionsRetrieved, setTransactionsRetrieved] = useState([]);
-  const [showCreate, setShowCreate] = useState(false)
-  const [description, setDescription] = useState('')
-  const [cost, setCost] = useState('')
-  const [splits, setSplits] = useState([])
+
+  const [manualSplit, setManualSplit] = useState(false);
+  const [showCreate, setShowCreate] = useState(false);
+  const [disableCreate, setDisableCreate] = useState(false);
+  const [dataRecieved, setDataRecieved] = useState(false);
+
+  const [description, setDescription] = useState('');
+  const [cost, setCost] = useState('');
 
   var lock = 0;
 
@@ -54,7 +67,21 @@ export default function transactions() {
     }
   }, [user, groupId]);
  
-  //Gets the users groups from the database
+   //Gets the users associated with groups
+   const getUsersFromGroup = async () => {
+    try {
+      const resp = await axios.post(
+        `/api/groups/getUsersFromGroup`,
+        { group_id: groupId },
+        { headers: { Authorization: `Bearer ${getToken(user)}` } }
+      );
+      setGroupUsers(resp.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  //Gets the transactions associtated with the group
   const getTransactions = async () => {
     try {
       const resp = await axios.post(
@@ -62,19 +89,33 @@ export default function transactions() {
         { group_id: groupId },
         { headers: { Authorization: `Bearer ${getToken(user)}` } },
       );
-      setTransactionsRetrieved(resp.data);
+      await getUsersFromGroup();
+      await setTransactionsRetrieved(resp.data);
+      setDataRecieved(true);
     } catch (err) {
       console.log(err);
-    }
+    } 
   }
 
   //Creates a new group associated with the user
   const createTransaction = async (e:  MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    if(groupUsersSplitting.length < 2) {
+      alert('You must split the transaction with at least 2 users');
+      return;
+    }
+    setDisableCreate(true);
     const splits: TransactionSplit[] = [];
-    const userId = getUserAttributes(user)?.sub
-    splits.push({user_id: userId, share: 100});
+    var sharePercent = 100 / groupUsersSplitting.length;
     
+    if (manualSplit) {
+      
+    } else {
+      groupUsersSplitting.forEach((user: any) => {
+        splits.push({user_id: user.id, share: sharePercent});
+      })
+    }
+
     try {
       const resp = await axios.post(
         `/api/transactions/create`,
@@ -89,12 +130,60 @@ export default function transactions() {
       console.log(resp);
       console.log(resp.status)
       if(resp.status === 201) {
-        getTransactions();  
+        getTransactions();
+        setGroupUsersSpltting([]);
         setShowCreate(false);
+        setDisableCreate(false);
       }
     } catch (err) {
       console.log(err);
     };
+  }
+    
+  useEffect(() => {
+    console.log(manualSplit)
+    
+  }, [manualSplit]);
+
+  const displayUsers = (display: any[], location: string) => {
+    let image = true;
+    if(display.length !== 0) {
+      return (
+        <div className='flex flex-row space-x-4'>
+          {display.map((user: any) => (
+            <div key={user.id} className='flex flex-col justify-center items-center'>
+              <img onClick={async () => {
+                handleSplit(user, location)
+              }} 
+              className="rounded-full border-2 hover:border-violet-600 hover:cursor-pointer" src={user.picture} alt={user.name} width="50px"/>
+              <div className='text-center'>{user.name}</div>
+            </div>
+          ))}
+        </div>
+      )
+    } else {
+      return (<></>)
+    } 
+  }
+
+  const handleSplit = (user: any, location: string) => {
+    if(location === 'ns') {
+    //User is not splitting so add them to the list
+      setGroupUsersSpltting([...groupUsersSplitting, user])
+      setGroupUsers(groupUsers.filter((filter:any) => {
+        if(user.id !== filter.id){
+          return true
+        }
+      }))
+    } else {
+      //User is splitting so remove them from the list
+      setGroupUsers([...groupUsers, user])
+      setGroupUsersSpltting(groupUsersSplitting.filter((filter:any) => {
+        if(user.id !== filter.id) {
+          return true;
+        }}
+      ))
+    }
   }
 
   //Shows the create group form
@@ -107,15 +196,23 @@ export default function transactions() {
       <h1>Current Transactions</h1>
       <div className="flex flex-col space-y-4">
         {/* Displays all the transactions that are part of this group */}
-        {transactionsRetrieved.map((transaction: Transaction) => (
-          <a 
-            className="flex flex-col space-y-4 border-cyan-800 hover:cursor-pointer border" 
-            key={transaction.id}
-          >
-            <h2>{transaction.description}</h2>
-            <p>Cost: {Number(transaction.cost).toFixed(2)}</p>
-          </a>
-        ))}
+        {dataRecieved ? (
+          transactionsRetrieved.map((transaction: Transaction) => (
+            <a 
+              className="flex flex-col space-y-4 border-cyan-800 hover:cursor-pointer border" 
+              key={transaction.id}
+            >
+              <h2>{transaction.description}</h2>
+              <p>Cost: {Number(transaction.cost).toFixed(2)}</p>
+              {/* display the users names from the userlist map that has a key of the group id */}
+              
+            </a>
+          ))
+         ) : (
+          <LoadingIcon style = {{transform: 'rotate(90deg)' }}/>
+         )}
+        
+        
       </div>
       <Button
         onClick={showTransaction}
@@ -123,7 +220,7 @@ export default function transactions() {
           Create a transaction
       </Button>
       {showCreate && (
-        <div>
+        <form>
           <TextField
             label="Description"
             placeholder="Description"
@@ -137,13 +234,28 @@ export default function transactions() {
             //set an on change to get the current value rounded to 2 decimal places
             onChange={(e: ChangeEvent<HTMLInputElement>) => setCost(e.target.value)}
           />
+          <SwitchField 
+            label="Split Manually" 
+            isChecked={manualSplit}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setManualSplit(e.target.checked)}
+          />
+          <div>
+            <h1>Not Splitting</h1>
+            {displayUsers(groupUsers, 'ns')}
+          </div>
+          <div>
+            <h1>Splitting</h1>
+            {displayUsers(groupUsersSplitting, 's')}
+          </div>
           <Button
             onClick={(e: MouseEvent<HTMLButtonElement>) => {createTransaction(e)}}
+            disabled={disableCreate}
           >
             Create Transaction
           </Button>
-        </div>
+        </form>
       )}
     </div>
   )
+
 }
